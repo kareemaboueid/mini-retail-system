d779296c778f28370e418e2d7973ca98776da580
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-04 13:08:04 +0300

    chore: Fix typo in user_create.ctrl.js

--- src/controllers/user/user_create.ctrl.js
+++ src/controllers/user/user_create.ctrl.js
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import { StatusCodes } from 'http-status-codes';
 import MDL_USER from '../../models/user/user.model.js';
-import hlp_jsend from '../../../helpers/jsend.hlp.js'
+import hlp_jsend from '../../../helpers/jsend.hlp.js';
 
 /**
  * ### Controller: CTRL_USER_CREATE
@@ -9,40 +9,51 @@ import hlp_jsend from '../../../helpers/jsend.hlp.js'
  * @route `/v1/users/create`
  */
 
-const CTRL_USER_CREATE= asyncHandler(async (_request, _response) => {
+const CTRL_USER_CREATE = asyncHandler(async (_request, _response) => {
   // ------ START ------ //
 
   try {
     // get data from client:
     const {
-first_name: eud_user_first_name,
-last_name: eud_user_last_name,
-email_address: eud_user_email_address,
-phone_number: eud_user_phone_number,
-job_position: eud_user_job_position,
-nationallity: eud_user_nationallity,
-civil_id: eud_user_civil_id,
-username: eud_user_username,
-password: eud_user_password,
-confirm_password: eud_user_confirm_password,
+      first_name: eud_user_first_name,
+      last_name: eud_user_last_name,
+      email_address: eud_user_email_address,
+      phone_number: eud_user_phone_number,
+      job_position: eud_user_job_position,
+      nationallity: eud_user_nationallity,
+      civil_id: eud_user_civil_id,
+      username: eud_user_username,
+      password: eud_user_password,
+      confirm_password: eud_user_confirm_password,
     } = _request.body;
 
-    // check required fields:
-    _request.checkBody('first_name', 'First name is required').notEmpty();
-    _request.checkBody('last_name', 'Last name is required').notEmpty();
-    _request.checkBody('email_address', 'Email address is required').notEmpty();
-    _request.checkBody('phone_number', 'Phone number is required').notEmpty();
-    _request.checkBody('job_position', 'Job position is required').notEmpty();
-    _request.checkBody('nationallity', 'Nationallity is required').notEmpty();
-    _request.checkBody('civil_id', 'Civil ID is required').notEmpty();
-    _request.checkBody('username', 'Username is required').notEmpty();
-    _request.checkBody('password', 'Password is required').notEmpty();
-    _request.checkBody('confirm_password', 'Confirm password is required').notEmpty();
+    // check if all required fields are provided:
+    if (
+      !eud_user_first_name ||
+      !eud_user_last_name ||
+      !eud_user_email_address ||
+      !eud_user_phone_number ||
+      !eud_user_job_position ||
+      !eud_user_nationallity ||
+      !eud_user_civil_id ||
+      !eud_user_username ||
+      !eud_user_password ||
+      !eud_user_confirm_password
+    ) {
+      _response.status(StatusCodes.BAD_REQUEST);
+      throw new Error('All fields are required');
+    }
+
+    // check if password and confirm password match:
+    if (eud_user_password !== eud_user_confirm_password) {
+      _response.status(StatusCodes.BAD_REQUEST);
+      throw new Error('Password and confirm password do not match');
+    }
 
     // database user query (not freeze):
     const USER = await MDL_USER.findOne({
       data_user_username: eud_user_username,
-    }).where({flag_user_freeze: false});
+    }).where({ flag_user_freeze: false });
 
     // check if user exists: (prevent duplicate)
     if (USER) {
@@ -50,26 +61,27 @@ confirm_password: eud_user_confirm_password,
       throw new Error('User already exists, try another username');
     }
 
-
     // create new user in database:
     await MDL_USER.create({
-data_user_first_name : eud_user_first_name,
-data_user_last_name : eud_user_last_name,
-data_user_email_address : eud_user_email_address,
-data_user_phone_number : eud_user_phone_number,
-data_user_job_position : eud_user_job_position,
-data_user_nationallity : eud_user_nationallity,
-data_user_civil_id : eud_user_civil_id,
-data_user_username : eud_user_username,
-data_user_password : eud_user_password,
+      data_user_first_name: eud_user_first_name,
+      data_user_last_name: eud_user_last_name,
+      data_user_email_address: eud_user_email_address,
+      data_user_phone_number: eud_user_phone_number,
+      data_user_job_position: eud_user_job_position,
+      data_user_nationallity: eud_user_nationallity,
+      data_user_civil_id: eud_user_civil_id,
+      data_user_username: eud_user_username,
+      data_user_password: eud_user_password,
     });
 
     // TODO save the new user info in secure httpOnly cookie w/ JWT:
 
     // send success response:
-    _response
-      .status(StatusCodes.CREATED)
-      .json(hlp_jsend(StatusCodes.CREATED, {message: `User ${eud_user_username} created successfully`}))
+    _response.status(StatusCodes.CREATED).json(
+      hlp_jsend(StatusCodes.CREATED, {
+        message: `User ${eud_user_username} created successfully`,
+      }),
+    );
 
     // ------ HANDLE ERRORS ------ //
   } catch (_error) {
