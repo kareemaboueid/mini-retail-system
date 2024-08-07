35283a2b508181857192866d1e6dfc93b1c78360
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-07 11:22:33 +0300

    refactor: Update database structure for users and companies

--- constants/db_strings.cnst.js
+++ constants/db_strings.cnst.js
@@ -1,29 +1,36 @@
-/**
- * ## User DB Structure
- * ```
- * coll: `users`
- * model: `User`
- * ```
- */
-const user = {
-  /** coll: `users` */
-  coll: String('users'),
-  /** model: `User` */
-  model: String('User'),
+/** ## Database Strings */
+const cnst_db_strings = () => {
+  const data = {
+    /**
+     * ## User DB Structure
+     * ```
+     * coll: `users`
+     * model: `User`
+     * ```
+     */
+    user: {
+      /** coll: `users` */
+      coll: String('users'),
+      /** model: `User` */
+      model: String('User'),
+    },
+
+    /**
+     * ## Company DB Structure
+     * ```
+     * coll: `companies`
+     * model: `Company`
+     * ```
+     */
+    company: {
+      /** coll: `companies` */
+      coll: String('companies'),
+      /** model: `Company` */
+      model: String('Company'),
+    },
+  };
+
+  return data;
 };
 
-/**
- * ## Company DB Structure
- * ```
- * coll: `companies`
- * model: `Company`
- * ```
- */
-const company = {
-  /** coll: `companies` */
-  coll: String('companies'),
-  /** model: `Company` */
-  model: String('Company'),
-};
-
-export { user, company };
+export default cnst_db_strings;
